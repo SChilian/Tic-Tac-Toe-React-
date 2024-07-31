521c38b722517097004de09a95cd1dc9f14e84fe
chilian_s <sierrachilian@gmail.com>
2024-07-30 23:58:23 -0400

    add custom CSS styling via prop to highlight which plaryer is actively playing and added prop to gameboard component for updating state

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,18 @@ function App() {
   const [isActive, setIsActive] = useState("X");
 
 function handleSelectSquare() {
-  setIsActive((currentlyIsActive) => currentlyIsActive === "X" ? "X" : "O")
+  setIsActive((currentlyIsActive) => currentlyIsActive === "X" ? "O" : "X")
 
 }
 
   return (
     <main>
       <div id="game-container">
-       <ol id="players">
-        <Player initialName="Player 1" symbol="X" />
-        <Player initialName="Player 2" symbol="O" />
+       <ol id="players" className="highlight-player">
+        <Player initialName="Player 1" symbol="X" isPlaying={isActive === "X"}/>
+        <Player initialName="Player 2" symbol="O" isPlaying={isActive === "O"}/>
        </ol>
-      <GameBoard onSelectSquare={handleSelectSquare}/>
+      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={isActive}/>
       </div>
         LOG
     </main>
