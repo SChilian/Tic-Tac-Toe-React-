74e7925bcd951c6125f026a289cb4431ef55d926
chilian_s <sierrachilian@gmail.com>
2024-07-30 22:37:57 -0400

    added function to udpate gameboard state

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -6,20 +9,25 @@ const initialGameBoard = [
 
 
 export default function GameBoard() {
+const [gameboard, setGameBoard] = useState(initialGameBoard);
+
+function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((previousGameBoard) => {
+        const updatedBoard = {...previousGameBoard.map(innerArray => [...innerArray])};
+        updatedBoard[rowIndex][colIndex] = "X";
+        return updatedBoard;
+    });
+}
+
     return <ol id="game-board">
         {initialGameBoard.map((row, rowIndex) => (
             <li key={rowIndex}>
             <ol>
                 {row.map((playerSymbol, colIndex) => (
                 <li key={colIndex}>
-                    <button>{playerSymbol}</button>
-                </li>
-                ))}
+                    <button onClick={() => handleSelectSquare(rowIndex, colIndex)}> {playerSymbol}</button>
+                </li>))}
             </ol>
-        </li>
-        ))}
-    </ol>
-    ;
-
-
+        </li>))}
+    </ol>;
 }
