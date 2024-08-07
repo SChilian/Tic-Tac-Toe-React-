b7abb95242a6d4689d5c19c013f6a3fba6c36d63
chilian_s <sierrachilian@gmail.com>
2024-08-06 22:36:29 -0400

    lifted state to manage data from the log component  in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,27 @@ import { useState } from "react";
 
 import Player from "./components/Player.jsx"
 import GameBoard from "./components/GameBoard.jsx"
+import Log from "./components/Log.jsx"
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([])
   const [isActive, setIsActive] = useState("X");
 
-function handleSelectSquare() {
-  setIsActive((currentlyIsActive) => currentlyIsActive === "X" ? "O" : "X")
+  function handleSelectSquare() {
+    setIsActive((currentlyIsActive) => currentlyIsActive === "X" ? "O" : "X")
 
-}
+  }
 
   return (
     <main>
       <div id="game-container">
-       <ol id="players" className="highlight-player">
-        <Player initialName="Player 1" symbol="X" isPlaying={isActive === "X"}/>
-        <Player initialName="Player 2" symbol="O" isPlaying={isActive === "O"}/>
-       </ol>
-      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={isActive}/>
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isPlaying={isActive === "X"} />
+          <Player initialName="Player 2" symbol="O" isPlaying={isActive === "O"} />
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={isActive} />
       </div>
-        LOG
+      <Log />
     </main>
   )
 }
